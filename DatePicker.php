7dc83cf23f32aaa93b9957c01a31be8b76f0eaab
Domenico <sviluppo@editt.it>
2017-09-11 14:41:15 +0200

    Initial commit

---
+++ DatePicker.php
@@ -0,0 +1,57 @@
+<?php
+namespace Model;
+
+class DatePicker extends Module {
+	/** @var array */
+	protected $options = [
+		'multilang' => false,
+	];
+
+	/**
+	 * @param array $options
+	 */
+	public function init($options){
+	    if(!$this->model->isLoaded('JQuery'))
+			$this->model->load('JQuery');
+		$this->options = array_merge($this->options, $options);
+	}
+
+	/**
+	 * Eventual headings for multilang websites
+	 */
+	public function headings(){
+		if($this->options['multilang']){
+			$dic = $this->model->_Multilang->getDictionary();
+			?>
+            <script>
+				var zebraDatePickerMonths = [
+					'<?=entities($dic['calendar-january'])?>',
+					'<?=entities($dic['calendar-february'])?>',
+					'<?=entities($dic['calendar-march'])?>',
+					'<?=entities($dic['calendar-april'])?>',
+					'<?=entities($dic['calendar-may'])?>',
+					'<?=entities($dic['calendar-june'])?>',
+					'<?=entities($dic['calendar-july'])?>',
+					'<?=entities($dic['calendar-august'])?>',
+					'<?=entities($dic['calendar-september'])?>',
+					'<?=entities($dic['calendar-october'])?>',
+					'<?=entities($dic['calendar-november'])?>',
+					'<?=entities($dic['calendar-december'])?>'
+				];
+				var zebraDatePickerDays = [
+					'<?=entities($dic['calendar-sunday'])?>',
+					'<?=entities($dic['calendar-monday'])?>',
+					'<?=entities($dic['calendar-tuesday'])?>',
+					'<?=entities($dic['calendar-wednesday'])?>',
+					'<?=entities($dic['calendar-thursday'])?>',
+					'<?=entities($dic['calendar-friday'])?>',
+					'<?=entities($dic['calendar-saturday'])?>'
+				];
+				var zebraDatePickerWeeks = '<?=entities($dic['calendar-weeks'])?>';
+				var zebraDatePickerToday = '<?=entities($dic['calendar-today'])?>';
+				var zebraDatePickerReset = '<?=entities($dic['calendar-reset'])?>';
+            </script>
+			<?php
+		}
+	}
+}
---
+++ files/check.js
@@ -0,0 +1,122 @@
+function checkDatePicker(id){
+	if(typeof id!='string'){
+		var arr = [];
+		var arr1 = document.querySelectorAll('input.datepicker_field'); // Retrocompatibilità
+		for(var i in arr1)
+			arr.push(arr1[i]);
+		if(window.innerWidth>=480){ // For smaller screen (aka smartphone) use only the default date plugin
+			var arr2 = document.querySelectorAll('input[type="date"]');
+			for(var i in arr2)
+				arr.push(arr2[i]);
+		}
+	}else{
+		var arr = [_(id)];
+	}
+
+	for(var i in arr){
+		if(typeof arr[i]!='object') continue;
+		if(arr[i].offsetParent===null) continue;
+
+		if(arr[i].getAttribute('type')=='date'){
+			if(isEdge()){
+				var v = arr[i].getValue();
+				arr[i].setAttribute('type', 'text');
+				arr[i].setValue(v, false);
+			}
+
+			if(isDateSupported() && !isEdge()){
+				var format = 'Y-m-d';
+			}else{
+				var format = 'd-m-Y';
+
+				var d = arr[i].getValue();
+				if(d){
+					d = new Date(d);
+					if(!isNaN(d.getTime())){
+						var day = d.getDate();
+						var month = d.getMonth()+1;
+						if(day<10) day = '0'+day;
+						if(month<10) month = '0'+month;
+						arr[i].value = day+'-'+month+'-'+d.getFullYear();
+					}
+				}
+			}
+		}else{
+			var format = 'd-m-Y';
+		}
+		if(arr[i].readOnly) continue;
+
+		attachDatePicker(arr[i], format);
+	}
+}
+
+function attachDatePicker(el, format){
+	if(typeof format=='undefined')
+		format = 'Y-m-d';
+
+	var obj = $(el);
+	var old_datepicker = obj.data('Zebra_DatePicker');
+	if(typeof old_datepicker=='undefined'){
+		el.setAttribute('autocomplete', 'off');
+		var options = {
+			show_icon: false,
+			format: format,
+			months: (typeof zebraDatePickerMonths!='undefined') ? zebraDatePickerMonths : ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+			readonly_element: false,
+			select_other_months: true,
+			show_week_number: (typeof zebraDatePickerWeeks!='undefined') ? zebraDatePickerWeeks : 'St.',
+			lang_clear_date: (typeof zebraDatePickerReset!='undefined') ? zebraDatePickerReset : 'Cancella',
+			show_select_today: (typeof zebraDatePickerToday!='undefined') ? zebraDatePickerToday : 'Oggi',
+			days: (typeof zebraDatePickerDays!='undefined') ? zebraDatePickerDays : ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'],
+			offset: [-el.offsetWidth, el.offsetHeight+255],
+			onSelect: function(){
+				if ("createEvent" in document) {
+					if(typeof this[0].dispatchEvent!='undefined'){
+						var evt = new Event('change', {"bubbles":false, "cancelable":true});
+						this[0].dispatchEvent(evt);
+					}
+				}else{
+					this[0].fireEvent("onchange");
+				}
+			},
+			onClear: function(){
+				if ("createEvent" in document) {
+					if(typeof this[0].dispatchEvent!='undefined'){
+						var evt = new Event('change', {"bubbles":false, "cancelable":true});
+						this[0].dispatchEvent(evt);
+					}
+				}else{
+					this[0].fireEvent("onchange");
+				}
+			}
+		};
+		var fine = false;
+		if(el.getAttribute('data-datepicker_fine'))
+			fine = el.getAttribute('data-datepicker_fine');
+		if(el.getAttribute('data-datepicker_end'))
+			fine = el.getAttribute('data-datepicker_end');
+		if(fine){
+			if(document.getElementById(fine))
+				options['pair'] = $('#'+fine);
+			else if(typeof el.form!='undefined' && typeof el.form[fine]!='undefined')
+				options['pair'] = $(el.form[fine]);
+		}
+		obj.Zebra_DatePicker(options);
+	}
+}
+
+function isEdge(){
+	return /Edge\/\d./i.test(navigator.appVersion);
+}
+
+window.addEventListener('load', function(){
+	checkDatePicker();
+
+	if (typeof MutationObserver !== 'undefined') {
+		var observer = new MutationObserver(function (mutations) {
+			checkDatePicker();
+		});
+
+		observer.observe(document.body, {"childList": true, "subtree": true});
+	}
+});
