9b6233ac264c4b368726b8d411af24cf104ca798
Domenico Giambra <zorinik@gmail.com>
2018-06-08 20:46:51 +0200

    Front end dependencies (css and js) are now auto loaded

--- DatePicker.php
+++ DatePicker.php
@@ -5,18 +5,6 @@ use Model\Core\Module;
 class DatePicker extends Module
 {
 	/**
-	 * @param array $options
-	 * @throws \Model\Core\Exception
-	 */
-	public function init(array $options)
-	{
-		if (!$this->model->isLoaded('JQuery'))
-			$this->model->load('JQuery');
-		if (!$this->model->isLoaded('FrontEnd'))
-			$this->model->load('FrontEnd');
-	}
-
-	/**
 	 * Eventual headings for multilang websites
 	 */
 	public function headings()
